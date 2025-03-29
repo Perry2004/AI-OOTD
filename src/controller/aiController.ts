0a676edd4ca062e97a1ca74bcae9499535181eb0
aric-chan <aricchan918@gmail.com>
2025-03-29 15:10:19 -0700

    gemini working

--- src/controller/aiController.ts
+++ src/controller/aiController.ts
@@ -1,34 +1,14 @@
 import { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } from "@google/generative-ai";
 
+const geminiApiKey:string = "";
 
-const geminiApiKey = process.env.GEMINI_API;
+
 const genAI = new GoogleGenerativeAI(geminiApiKey);
 
-const model = genAI.getGenerativeModel({
-    model: "gemini-2.0-flash",
-});
 
-const generationConfig = {
-    temperature: 1,
-    topP: 0.95,
-    topK: 64,
-    maxOutputTokens: 8192,
-    responseMimeType: "text/plain",
-};
-
-export async function runGemini(prompt) {
-    const chatSession = model.startChat({
-        generationConfig,
-// safetySettings: Adjust safety settings
-// See https://ai.google.dev/gemini-api/docs/safety-settings
-        history: [
-        ],
-    });
-
-    if (typeof prompt !== "string") {
-        throw new Error("Invalid prompt: Expected a string");
-    }
-
-    const result = await chatSession.sendMessage(prompt);
-    return result.response.text();
-};
+// Reusable gemini AI
+export async function generateAIContent(prompt) {
+    const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
+    const result = await model.generateContent(prompt);
+    return await result.response.text();
+}
