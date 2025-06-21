057786c0b88f11cf6fc7a8f4b986709ca18914e9
Perry <perryzhu2004@outlook.com>
2025-06-20 22:06:47 -0700

    feat: setup spring boot project with basic structure

---
+++ backend/ai-ootd/src/main/java/net/perryz/ai_ootd/AiOotdApplication.java
@@ -0,0 +1,13 @@
+package net.perryz.ai_ootd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiOotdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiOotdApplication.class, args);
+	}
+
+}
---
+++ backend/ai-ootd/src/test/java/net/perryz/ai_ootd/AiOotdApplicationTests.java
@@ -0,0 +1,13 @@
+package net.perryz.ai_ootd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiOotdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
